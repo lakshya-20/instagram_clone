2af0fe7545c26607b7509f33f0f51206c054363e
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-05-08 18:33:47 +0530

    Setting up mongodb

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+const url=require('./key');
+var connect=mongoose.connect(url);
+connect.then((db) =>{
+  console.log('Connected correctly to mongodb');
+},(err)=>{console.log(err)});
+
 const app=express()
 const PORT=5000
-
 
 app.get('/',(req,res)=>{
     res.send("Hello World")
