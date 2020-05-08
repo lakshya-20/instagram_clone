ab8ccff4a5573638ed7d5af8ccfaa407ef1ae9cc
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-05-09 01:13:01 +0530

    Login/SignUp UI

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 
 const Login =()=>{
     return(
-        <h1>Login</h1>
+        <div className="mycard">
+            <div className="card auth-card input-field">
+                <h2>Instagram</h2>
+                <input type="text" placeholder="email" />
+                <input type="text" placeholder="password" />
+                <button class="btn waves-eddect waves-light #64b5f6 blue lighten-2">
+                    Login
+                </button>
+                <h5>
+                    <Link to="/signup">Dont have an account ?</Link>
+                </h5>
+            </div>
+        </div>
     )
 }
 export default Login;
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-
-const Signup =()=>{
+const Login =()=>{
     return(
-        <h1>Signup</h1>
+        <div className="mycard">
+            <div className="card auth-card input-field">
+                <h2>Instagram</h2>
+                <input type="text" placeholder="name" />
+                <input type="text" placeholder="email" />
+                <input type="text" placeholder="password" />
+                <button class="btn waves-eddect waves-light #64b5f6 blue lighten-2">
+                    SignUP
+                </button>
+                <h5>
+                    <Link to="/login">Already have an account ?</Link>
+                </h5>
+            </div>
+        </div>
     )
 }
-
-export default Signup;
+export default Login;
