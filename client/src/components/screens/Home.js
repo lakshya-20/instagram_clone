3b407fb2486db6603d3b6da2fea48f4272da6bf8
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-05-09 22:55:50 +0530

    Fetching posts from server

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,22 +1,44 @@
-import React, {useState,useEffect} from 'react'
-
+import React, {useState,useEffect,useContext} from 'react'
+import {usercontext} from '../../App'
+import {Link} from 'react-router-dom'
 
 const Home =()=>{
-    
+    const[data,setData]=useState([])
+    useEffect(()=>{
+        fetch('/allpost',{
+            headers:{
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            }
+        }).then(res=>res.json())
+        .then(result=>{
+            setData(result.posts)
+            console.log("lkjlk",result.posts)
+        })
+    },[])
     return(
         <div className="home">
-            <div className="card home-card">
-                <h5>Lakshya</h5>
-                <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
-                </div>
-                <div className="card-content">
-                    <i className="material-icons" style={{color:"red"}}>favorite</i>
-                    <h6>title</h6>
-                    <p>this is amazing post</p>
-                    <input type="text" placeholder="add a comment" />
-                </div>
-            </div>
+
+            {
+                data.map(item=>{
+                    return(
+                        <div className="card home-card" key={item._id}>
+                            <h5>{item.postedBy.name}</h5>
+                            <div className="card-image">
+                                <img src={item.photo}/>
+                            </div>
+                            <div className="card-content">
+                                <i className="material-icons" style={{color:"red"}}>favorite</i>
+                                <h6>{item.title}</h6>
+                                <p>{item.body}</p>
+                                <input type="text" placeholder="add a comment" />
+                            </div>
+                        </div>
+                    )
+                })
+            }
+
+
+            
         </div>
     )
 }
