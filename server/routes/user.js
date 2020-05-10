843245bcaf4a2d38471f1e145586f8daac88fbde
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-05-10 20:27:26 +0530

    unfollow user api

--- server/routes/user.js
+++ server/routes/user.js
@@ -46,5 +46,26 @@ router.put('/follow',requireLogin,(req,res)=>{
     )
 })
 
+router.put('/unfollow',requireLogin,(req,res)=>{
+    User.findByIdAndUpdate(req.body.unfollowId,{
+        $pull:{followers:req.user._id}
+    },{
+        new:true
+    },(err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }
+      User.findByIdAndUpdate(req.user._id,{
+          $pull:{following:req.body.unfollowId}
+          
+      },{new:true}).select("-password").then(result=>{
+          res.json(result)
+      }).catch(err=>{
+          return res.status(422).json({error:err})
+      })
+
+    }
+    )
+})
 
 module.exports=router
