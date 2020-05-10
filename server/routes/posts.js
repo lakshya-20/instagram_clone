0faadb93072bd01da75a28f98de0b0654289290b
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-05-11 00:09:58 +0530

    following posts api

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -117,4 +117,19 @@ router.delete('/deletepost/:postId',requireLogin,(req,res)=>{
     })
 })
 
+router.get('/getsubpost',requireLogin,(req,res)=>{
+
+    // if postedBy in following
+    Post.find({postedBy:{$in:req.user.following}})
+    .populate("postedBy","_id name")
+    .populate("comments.postedBy","_id name")
+    .sort('-createdAt')
+    .then(posts=>{
+        res.json({posts})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
 module.exports=router
