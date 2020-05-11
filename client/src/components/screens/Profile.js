cf55d46dab46c1f9d63f356f62ff4e7f83945ef7
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-05-11 12:46:31 +0530

    react update dp

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -4,6 +4,7 @@ import {usercontext} from '../../App'
 const Profile =()=>{
     const [mypics,setPics]=useState([])
     const {state,dispatch}=useContext(usercontext)
+    const [image,setImage] = useState("")
     useEffect(()=>{
         fetch('/mypost',{
             headers:{
@@ -15,6 +16,46 @@ const Profile =()=>{
             console.log(result.mypost)
         })
     },[])
+
+    useEffect(()=>{
+        if(image){
+         const data = new FormData()
+         data.append("file",image)
+        data.append('upload_preset',"insta_clone");
+        data.append("cloud_name","dstmsi8qv")
+        fetch('	https://api.cloudinary.com/v1_1/dstmsi8qv/image/upload',{
+             method:"post",
+             body:data
+         })
+         .then(res=>res.json())
+         .then(data=>{
+            fetch('/updatepic',{
+                method:"put",
+                headers:{
+                    "Content-Type":"application/json",
+                    "Authorization":"Bearer "+localStorage.getItem("jwt")
+                },
+                body:JSON.stringify({
+                    pic:data.url
+                })
+            }).then(res=>res.json())
+            .then(result=>{
+                console.log(result)
+                localStorage.setItem("user",JSON.stringify({...state,pic:result.pic}))
+                dispatch({type:"UPDATEPIC",payload:result.pic})
+            })
+        
+         })
+         .catch(err=>{
+             console.log(err)
+         })
+        }
+     },[image])
+     const updatePhoto = (file)=>{
+         setImage(file)
+     }
+
+
     return(
         <div style={{maxWidth:"1000px",margin:"0px auto  "}}>
             <div style={{
@@ -49,7 +90,7 @@ const Profile =()=>{
             <div className="file-field input-field" style={{margin:"10px"}}>
             <div className="btn #64b5f6 blue darken-1">
                 <span>Update pic</span>
-                <input type="file" />
+                <input type="file" onChange={(e)=>updatePhoto(e.target.files[0])} />
             </div>
             <div className="file-path-wrapper">
                 <input className="file-path validate" type="text" />
